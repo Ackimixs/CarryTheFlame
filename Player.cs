ee8d370991033741d39c529b5e5b87c82e44e1ed
romain.cro <romain.croguennoc@isen-ouest.yncrea.fr>
2025-12-16 16:13:37 +0100

    weapon texture + bullet

--- Player.cs
+++ Player.cs
@@ -12,14 +12,12 @@ public partial class Player : CharacterBody3D
 	private float gravity = 20f;
 	[Export]
 	private float jumpVelocity = 10f;
-
 	
 	public override void _Ready()
 	{
 		camera = GetNode<Camera3D>("%Camera3D");
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
-
 	
 	public override void _UnhandledInput(InputEvent e)
 	{
@@ -32,7 +30,6 @@ public partial class Player : CharacterBody3D
 					camera.RotationDegrees.Z
 			);
 		}
-
 		if (e.IsActionPressed("ui_cancel"))
 		{
 			Input.MouseMode = Input.MouseModeEnum.Visible;
@@ -42,24 +39,18 @@ public partial class Player : CharacterBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 inputDirection2D = Input.GetVector("move_left", "move_right", "move_back", "move_forward");
-		// When inputDirection.X is < 0 (move_left), the player needs to go towards -X
-		// When inputDirection.Y is > 0 (move_forward), the player needs to go towards -Z
 		Vector3 inputDirection3D = new Vector3(
 			inputDirection2D.X, 0.0f, -inputDirection2D.Y
 		);
 
 		Vector3 playerVelocity = Transform.Basis * inputDirection3D * speed;
-
 		playerVelocity.Y = Velocity.Y - gravity * (float)delta;
-
+		
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
 			playerVelocity.Y = jumpVelocity;
 		}
-
 		Velocity = playerVelocity;
 		MoveAndSlide();
 	}
-
-
 }
