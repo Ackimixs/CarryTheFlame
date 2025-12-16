3aa388a814a9a9d1a7a7a9e370295ac8b0035395
PAITIER Mathias <mpaitier44@gmail.com>
2025-12-16 16:32:06 +0100

    [ADD] Steps Climb

--- Player.cs
+++ Player.cs
@@ -1,6 +1,14 @@
 using Godot;
 using System;
 
+public class StepResult
+{
+    public Vector3 DiffPosition = Vector3.Zero;
+    public Vector3 Normal = Vector3.Zero;
+    public bool IsStepUp = false;
+}
+
+
 public partial class Player : CharacterBody3D
 {
 	[Export]
@@ -12,6 +20,9 @@ public partial class Player : CharacterBody3D
 	private float gravity = 20f;
 	[Export]
 	private float jumpVelocity = 10f;
+	[Export] public float StepHeight = 0.6f;
+	[Export] public float MaxStepSlope = 40.0f;
+	[Export] public int StepCheckCount = 2;
 
 	
 	public override void _Ready()
@@ -41,25 +52,87 @@ public partial class Player : CharacterBody3D
 	
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 inputDirection2D = Input.GetVector("move_left", "move_right", "move_back", "move_forward");
-		// When inputDirection.X is < 0 (move_left), the player needs to go towards -X
-		// When inputDirection.Y is > 0 (move_forward), the player needs to go towards -Z
-		Vector3 inputDirection3D = new Vector3(
-			inputDirection2D.X, 0.0f, -inputDirection2D.Y
-		);
+		Vector2 input = Input.GetVector("move_left", "move_right", "move_back", "move_forward");
+		Vector3 direction = (Transform.Basis * new Vector3(input.X, 0, -input.Y)).Normalized();
 
-		Vector3 playerVelocity = Transform.Basis * inputDirection3D * speed;
+		Vector3 horizontalVelocity = direction * speed;
 
-		playerVelocity.Y = Velocity.Y - gravity * (float)delta;
+		Vector3 velocity = Velocity;
+		velocity.Y -= gravity * (float)delta;
 
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = jumpVelocity;
+
+		StepResult stepResult = new StepResult();
+		if (StepCheck(delta, horizontalVelocity, stepResult))
 		{
-			playerVelocity.Y = jumpVelocity;
+			GlobalTransform = GlobalTransform.Translated(stepResult.DiffPosition);
+			velocity.Y = 0;
 		}
 
-		Velocity = playerVelocity;
+		velocity.X = horizontalVelocity.X;
+		velocity.Z = horizontalVelocity.Z;
+
+		Velocity = velocity;
 		MoveAndSlide();
 	}
 
+	private bool StepCheck(double delta, Vector3 horizontalVelocity, StepResult result)
+	{
+		if (!IsOnFloor())
+			return false;
+
+		Vector3 stepHeightVec = Vector3.Up * StepHeight;
+		Vector3 stepIncrement = stepHeightVec / StepCheckCount;
+
+		for (int i = 0; i < StepCheckCount; i++)
+		{
+			Vector3 stepOffset = stepHeightVec - stepIncrement * i;
+
+			// 1️⃣ Test UP
+			Transform3D upTransform = GlobalTransform;
+			upTransform.Origin += stepOffset;
+
+			// 2️⃣ Test FORWARD
+			var forwardParams = new PhysicsTestMotionParameters3D
+			{
+				From = upTransform,
+				Motion = horizontalVelocity * (float)delta
+			};
+
+			var forwardResult = new PhysicsTestMotionResult3D();
+
+			if (PhysicsServer3D.BodyTestMotion(GetRid(), forwardParams, forwardResult))
+				continue;
+
+			upTransform.Origin += forwardParams.Motion;
+
+			// 3️⃣ Test DOWN
+			var downParams = new PhysicsTestMotionParameters3D
+			{
+				From = upTransform,
+				Motion = -stepOffset
+			};
+
+			var downResult = new PhysicsTestMotionResult3D();
+
+			if (PhysicsServer3D.BodyTestMotion(GetRid(), downParams, downResult))
+			{
+				float slopeAngle = downResult
+					.GetCollisionNormal()
+					.AngleTo(Vector3.Up);
+
+				if (slopeAngle <= Mathf.DegToRad(MaxStepSlope))
+				{
+					result.IsStepUp = true;
+					result.DiffPosition = -downResult.GetRemainder();
+					result.Normal = downResult.GetCollisionNormal();
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
 
 }
